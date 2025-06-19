5af63e29fbcfce1e16ce811dd2d4362b924da3af
Fitsum1101 <fitsumt835@gmail.com>
2025-06-19 17:09:54 +0300

    building the home page

--- Front-end/src/pages/Home.jsx
+++ Front-end/src/pages/Home.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import Navbar from "../components/Navbar";
+
+import Hero from "../components/Hero";
+
+import Footer from "../components/Footer";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
