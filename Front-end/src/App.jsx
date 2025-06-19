2e553b1e04c25eb4f6daf1bd532305440b1f501e
Fitsum1101 <fitsumt835@gmail.com>
2025-06-19 17:10:52 +0300

    building the route

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Home from "./pages/Home";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+//  create me a route that will render the App component
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <div>Error: Page not found</div>,
+  },
+]);
 
 const App = () => {
-  return <div className="text-4xl text-blue-700 text-center ">App</div>;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 };
 
 export default App;
