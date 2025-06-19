0984e11c65ca02a682105060ab876941e8f8865a
Fitsum1101 <fitsumt835@gmail.com>
2025-06-19 17:08:45 +0300

    starting the hero section

---
+++ Front-end/src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import banner from "../assets/banners/banner1.jpg";
+
+const Hero = () => {
+  return (
+    <div
+      className=" bg-cover bg-center h-[70vh] w-full "
+      style={{
+        backgroundImage: `url('${banner}')`,
+      }}
+    >
+      <div className="flex flex-col pl-4 mx-auto my-auto  max-w-[60rem] bg-red-500  justify-center text-white">
+        <p className="text-[1rem]">
+          working since 1999
+          <span className="inline-block h-[.2rem] w-[3rem] self-center ml-1 bg-red-500"></span>
+        </p>
+        <h1 className="text-5xl font-bold tracking-normal">
+          Tuneup Your Car <br />
+          to Next Level
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
