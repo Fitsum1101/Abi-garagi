ef05775abf0cecddf06af20d39fff2fe229f37ff
Fitsum1101 <fitsumt835@gmail.com>
2025-06-20 21:42:58 +0300

    building the services compenet

--- Front-end/src/pages/Services.jsx
+++ Front-end/src/pages/Services.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import Banner from "../components/banner/Banner";
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div>
+      <Banner activeNav="Services" />
+      <div className="p-4">
+        <h1 className="text-2xl font-bold">Our Services</h1>
+        <p className="mt-2">
+          We offer a wide range of services to meet your needs. From web
+          development to digital marketing, our team is here to help you
+          succeed.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Services;
