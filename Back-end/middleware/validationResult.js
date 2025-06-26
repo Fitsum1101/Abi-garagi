4c57b991468eb1b168b6de8d96bcf31cac148529
Fitsum1101 <fitsumt835@gmail.com>
2025-06-26 21:49:28 +0300

    validation result middleware created successfully

---
+++ Back-end/middleware/validationResult.js
@@ -0,0 +1,18 @@
+const { error } = require("console");
+const { validationResult } = require("express-validator");
+
+exports.validateResultMiddle = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const firstError = errors.array()[0];
+    console.log(firstError);
+    return res.status(400).json({
+      error: {
+        name: firstError.path,
+        msg: firstError.msg,
+      },
+    });
+  }
+  next();
+};
