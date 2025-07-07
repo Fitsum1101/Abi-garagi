5bcc32330ad9b7faa67290f7ccd6a2f42913250d
Fitsum1101 <fitsumt835@gmail.com>
2025-07-07 22:06:58 +0300

    adding role based access controller

---
+++ Back-end/middleware/authroizeRole.js
@@ -0,0 +1,8 @@
+module.exports = authorizeRole = (role) => {
+  return (req, res, next) => {
+    if (req.user.role === role || req.user.role === "ADMIN") next();
+    return res
+      .status(403)
+      .json({ message: "Access denied. Insufficient permissions." });
+  };
+};
