953669c10a1242e590bbf6980eef8cb057f73bcb
Fitsum1101 <fitsumt835@gmail.com>
2025-07-09 22:04:09 +0300

    adding role authrization

--- Back-end/middleware/authroizeRole.js
+++ Back-end/middleware/authroizeRole.js
@@ -1,6 +1,8 @@
 module.exports = authorizeRole = (role) => {
   return (req, res, next) => {
-    if (req.user.role === role || req.user.role === "ADMIN") next();
+    if (req.user.role === role || req.user.role === "ADMIN") {
+      return next();
+    }
     return res
       .status(403)
       .json({ message: "Access denied. Insufficient permissions." });
