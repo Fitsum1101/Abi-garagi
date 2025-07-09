24ec11d265e45133688c892655c243f122000b86
Fitsum1101 <fitsumt835@gmail.com>
2025-07-09 22:03:31 +0300

    adding authtication for the routes

--- Back-end/middleware/authenticateToken.js
+++ Back-end/middleware/authenticateToken.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 exports.authenticateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
-  // const token = authHeader && authHeader.split(" ")[1];
+  const token = authHeader && authHeader.split(" ")[1];
   if (!token) {
     return res
       .status(401)
@@ -12,6 +12,7 @@ exports.authenticateToken = (req, res, next) => {
     if (err) {
       return res.status(403).json({ message: "Invalid or expired token" });
     }
+    console.log("================", user);
     req.user = user;
     next();
   });
